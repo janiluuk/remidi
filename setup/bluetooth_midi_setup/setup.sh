9bbefb5c201ec93e37562b2d6d5a03c0c1efdee8
Jani Luukkanen <janiluuk@gmail.com>
2021-02-25 22:33:28 +0200

    Additional files

--- setup/bluetooth_midi_setup/setup.sh
+++ setup/bluetooth_midi_setup/setup.sh
@@ -1,5 +1,16 @@
-sudo cp -R ./system/* /
+#!/bin/sh
 
+git clone https://github.com/oxesoft/bluez
+sudo apt-get install -y autotools-dev libtool autoconf
+sudo apt-get install -y libasound2-dev
+sudo apt-get install -y libusb-dev libdbus-1-dev libglib2.0-dev libudev-dev libical-dev libreadline-dev
+cd bluez
+./bootstrap
+./configure --enable-midi --prefix=/usr --mandir=/usr/share/man --sysconfdir=/etc --localstatedir=/var
+make
+sudo make install
+cd ..
+sudo cp -R ./system/* /
 sudo udevadm control --reload
 sudo service udev restart
 sudo systemctl daemon-reload
